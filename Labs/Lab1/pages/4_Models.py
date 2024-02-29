4ce26ff5713519792f050abbadcf9cc3a245f1d9
Святослав Салабун <124811370+RabbiPaw@users.noreply.github.com>
2024-02-29 23:23:18 +0600

    Update 4_Models.py

--- Labs/Lab1/pages/4_Models.py
+++ Labs/Lab1/pages/4_Models.py
@@ -6,11 +6,11 @@ import pandas as pd
 st.set_page_config(page_title = "Как создавались модели")
 st.title('Показ создания моделей')
 
-st.image('2.1.jpg', caption='Подготовка данных, разделение на тестовую и обучающую выборки')
-st.image('2.2.jpg', caption='Nearest Neighbors Classification')
-st.image('2.3.jpg', caption='Подбор гиперпараметра')
-st.image('2.4.jpg', caption='Конечная модель, выгрузка модели')
-st.image('2.5.jpg', caption='Полиномиальная регрессия, выгрузка модели')
+st.image('Labs/Lab1/2.1.jpg', caption='Подготовка данных, разделение на тестовую и обучающую выборки')
+st.image('Labs/Lab1/2.2.jpg', caption='Nearest Neighbors Classification')
+st.image('Labs/Lab1/2.3.jpg', caption='Подбор гиперпараметра')
+st.image('Labs/Lab1/2.4.jpg', caption='Конечная модель, выгрузка модели')
+st.image('Labs/Lab1/2.5.jpg', caption='Полиномиальная регрессия, выгрузка модели')
 
 
 st.write('------------------------------------------------------------------------------------------------------------------------------------------')
