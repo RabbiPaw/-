3e6a85fcc3a96eb26699adb04cf653aedd6f6799
Святослав Салабун <124811370+RabbiPaw@users.noreply.github.com>
2024-02-29 23:35:47 +0600

    Update 2_Handling.py

--- Labs/Lab1/pages/2_Handling.py
+++ Labs/Lab1/pages/2_Handling.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title = "Предобработка данных")
-st.title('Фотографии и ссылка на код, где были обработаны взянные данные')
+st.title('Фотографии и ссылка на код, где были обработаны взятые данные')
 
 st.image('Labs/Lab1/1.jpg', caption='Считывание датасета')
 st.image('Labs/Lab1/2.jpg', caption='Discribe, Info')
